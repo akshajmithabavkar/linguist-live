f56714a7206fe5cf117a792bdbd2cd406d5859d1
Akshaj <akshajtanisha1319@gmail.com>
2026-04-09 02:27:29 +0530

    final cleanup of config and ghost files

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/react-swc'; export default defineConfig({ plugins: [react()], base: '/' });
+import { defineConfig } from 'vite'; export default defineConfig({ base: '/' });

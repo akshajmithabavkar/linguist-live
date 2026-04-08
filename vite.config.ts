f1eaddb72c09655652449df78509b877e37017ba
Akshaj <akshajtanisha1319@gmail.com>
2026-04-09 03:03:48 +0530

    fix: force relative paths for assets and scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import { defineConfig } from 'vite'; export default defineConfig({ base: '/' });
+import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react-swc'; export default defineConfig({ plugins: [react()], base: './' });
